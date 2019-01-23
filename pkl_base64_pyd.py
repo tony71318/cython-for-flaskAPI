789b949edc0f15c5dc55e89088a8c84a7290814d
Tony <tony71318@gmail.com>
2019-01-23 11:29:22 +0800

    direct compile on linux

--- pkl_base64_pyd.py
+++ pkl_base64_pyd.py
@@ -66,7 +66,7 @@ if platform.system() == 'Windows':
         else:
             print('something wrong')
 
-        id_pyd_dir = "./web_taipower/py/pkl/" + _id
+        id_pyd_dir = "./" + _id
         if not os.path.isdir(id_pyd_dir):
             os.mkdir(id_pyd_dir)
             print("make dir for: " + _id)
@@ -79,10 +79,67 @@ if platform.system() == 'Windows':
             print('something wrong')
 else:
     for pkl_name in pkl_name_list:
-        # ex. gcc -shared -o libhello.so -fPIC hello.c
-        process = subprocess.Popen('gcc -shared -o ../flaskAPI/py/pkl/'+ _id + '/' + pkl_name + '.so -fPIC ./c/' + _id + '/' + pkl_name + '.c', shell=True, stdout=subprocess.PIPE)
+        with open('pkl/' + pkl_name + '.pkl', 'rb') as f:
+            byteArray = f.read()
+
+        enc_byteArray = base64.b64encode(byteArray)
+
+        with open(pkl_name + '.py', 'w') as f:
+            f.write('#cython: language_level=3\n')
+            if pkl_name == 'features':
+                f.write('enc_features = ' + str(enc_byteArray))
+            elif pkl_name == 'num_previous':
+                f.write('enc_num_previous = ' + str(enc_byteArray))
+            else:
+                f.write('enc_model = ' + str(enc_byteArray))
+
+            f.close()
+        
+        process = subprocess.Popen('python3 setup.py ' + pkl_name + ' build_ext --inplace', shell=True, stdout=subprocess.PIPE)
         process.wait()
         if process.returncode == 0:
-            print('compile ' + pkl_name + '.so sucess.')
+            print('build pyd success.')
         else:
             print('something wrong')
+
+        process = subprocess.Popen('rm ' + pkl_name + '.py', shell=True, stdout=subprocess.PIPE)
+        process.wait()
+        if process.returncode == 0:
+            print('delete ' + pkl_name + '.py sucess.')
+        else:
+            print('something wrong')
+
+       	process = subprocess.Popen('rm ' + pkl_name + '.c ', shell=True, stdout=subprocess.PIPE)
+        process.wait()
+        if process.returncode == 0:
+            print('remove .c sucess')
+        else:
+            print('something wrong')
+
+        process = subprocess.Popen('mv ' + pkl_name + '.*.so ' + pkl_name + '.so', shell=True, stdout=subprocess.PIPE)
+        process.wait()
+        if process.returncode == 0:
+            print('rename ' + pkl_name + '.so sucess.')
+        else:
+            print('something wrong')
+
+        id_dir = "./" + _id
+        if not os.path.isdir(id_dir):
+            os.mkdir(id_dir)
+            print("make dir for: " + _id)
+
+        process = subprocess.Popen('mv ' + pkl_name + '.so ' + id_dir, shell=True, stdout=subprocess.PIPE)
+        process.wait()
+        if process.returncode == 0:
+            print('move ' + pkl_name + '.so sucess.')
+        else:
+            print('something wrong')
+
+
+       	## ex. gcc -shared -o libhello.so -fPIC hello.c
+        #process = subprocess.Popen('gcc -shared -o ../flaskAPI/py/pkl/'+ _id + '/' + pkl_name + '.so -fPIC ./c/' + _id + '/' + pkl_name + '.c', shell=True, stdout=subprocess.PIPE)
+        #process.wait()
+        #if process.returncode == 0:
+        #    print('compile ' + pkl_name + '.so sucess.')
+        #else:
+	        #    print('something wrong')
